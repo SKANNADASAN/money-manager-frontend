1a6b6293e8a1f49057018f0938310e4386c53b05
AnithaG-96 <anithagk96@gmail.com>
2024-01-04 04:14:43 +0530

    commit

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-light p-4">
+      <h6 className="text-center text-light">
+        All rights reserved &copy; Kannadasan
+      </h6>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/global.js
@@ -0,0 +1,2 @@
+export const API = "https://money-manager-backend-rho.vercel.app/api/v1";
+
